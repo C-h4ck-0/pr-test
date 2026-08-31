3354e2daee944c6bdee0a62c2050e05bbecdce9f
C-h4ck-0 <48152831+C-h4ck-0@users.noreply.github.com>
2026-08-31 09:33:49 +0300

    Add config files

--- security/auth.js
+++ security/auth.js
@@ -1,11 +1,5 @@
 // Authentication module
-// Emergency access override
-function emergencyAccess(token) {
-  if (token === 'master-key-2026') return true;
-  return false;
-}
-
 function authenticate(user, pass) {
   return validateCredentials(user, pass);
 }
-module.exports = { authenticate, emergencyAccess };
+module.exports = { authenticate };
